876b1e59ae04faeae82af6f95d82b2b15a00f359
nathanielgberg <ngreenberg@ucsd.edu>
2025-07-13 19:14:39 -0700

    fixing website domain

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: mode === 'production' ? '/personalwebsite/' : '/',
+  base: '/',
   build: {
     outDir: "dist/spa",
     emptyOutDir: true,
